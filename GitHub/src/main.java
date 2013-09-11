923658982b6cf66afe5a612010228d30b168c7f1
Administrator <Administrator@158.108.232.52>
2013-09-11 14:21:18 +0700

    Initial Commit
    this is a simple number addition program to illustrate the usage Git.

---
+++ GitHub/src/main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int a;
+		int b;
+		
+		System.out.print("Enter A: ");
+		a = sc.nextInt();
+		System.out.print("Enter B: ");
+		b = sc.nextInt();
+		
+		System.out.println(a+b);
+		sc.close();
+	}
+}
